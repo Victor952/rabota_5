916adbf8e833387aa0be20d8664566297e717de1
Victor <vitya.chernykh.2001@mail.ru>
2025-10-04 19:28:20 +0700

    Initial version with PEP8 errors

--- converter.py
+++ converter.py
@@ -1,30 +1,30 @@
 def convert_temperature(temp, unit):
-if unit == 'C':
-f = (temp * 9/5) + 32
-k = temp + 273.15
-return {'F'
-,'K'
-}
-elif unit == 'F':
-c = (temp - 32) * 5/9
-k = c + 273.15
-return {'C':   c, 'K'
-}
-elif unit == 'K':
-c = temp - 273.15
-f = (c * 9/5) + 32
-return {'C'
-, 'F': f}
-else:
-return 'Unsupported unit'
+    if unit == 'C':
+        f = (temp * 9/5) + 32
+        k = temp + 273.15
+        return {'F': f, 'K': k}
+    elif unit == 'F':
+        c = (temp - 32) * 5/9
+        k = c + 273.15
+        return {'C': c, 'K': k}
+    elif unit == 'K':
+        c = temp - 273.15
+        f = (c * 9/5) + 32
+        return {'C':, 'F': f}
+    else:
+        return 'Unsupported unit'
 
 def main():
-data = [
-(100, 'C'),
-(32, 'F'),
-(300, 'K'),
-(40, 'X') ]
-for temp, unit in data:
-result = convert_temperature(temp, unit)
-print(f'Input: {temp}°{unit} -> Output: {result}')
-main()
+    data = [
+        (100, 'C'),
+        (32, 'F'),
+        (300, 'K'),
+        (40, 'X')
+    ]
+    for temp, unit in data:
+        result = convert_temperature(temp, unit)
+        print(f'Input: {temp}°{unit} -> Output: {result}')
+
+
+if name == "main":
+    main()
